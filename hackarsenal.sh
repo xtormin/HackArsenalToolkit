478b6941789cdaefc6323ff6a81772394092e09c
j <j>
2020-09-02 20:41:09 +0100

    py & bash

--- hackarsenal.sh
+++ hackarsenal.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+source src/bashutils/banner.sh
 source src/core.sh
 
+### BANNER (I know, this is obvious xD)
+banner
+
+### PYTHON SETUP
+python_setup
+
+### CREATE AND PARSE INFORMATION
+python3 src/brain.py
+
+### DOWNLOAD AND INSTALL PROGRAMS
 main
