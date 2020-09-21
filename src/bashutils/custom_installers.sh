ed1cc5ad1b6d2d772d07ba4721d4c3b7c949d243
j <j>
2020-09-21 17:23:44 +0100

    More tools

--- src/bashutils/custom_installers.sh
+++ src/bashutils/custom_installers.sh
@@ -3,3 +3,10 @@ wpscan_installer(){
     sudo gem install bundler && bundle config set without 'test development'
     cd $SCRIPT_PATH
 }
+
+i_theHarvester(){
+    cd $TOOLS_PATH/theHarvester/
+    python3 -m pip install -r requirements/base.txt
+    sudo python3 setup.py install
+    cd $SCRIPT_PATH
+}
